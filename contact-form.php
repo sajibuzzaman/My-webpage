d9372dcd9c41e3bbf21fe9870fe2c7a0d2b254b8
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-02-15 02:21:08 +0600

    first commit

---
+++ contact-form.php
@@ -0,0 +1,19 @@
+﻿<?php 
+	$name = $_POST['Name'];
+	$visitor_email = $_POST['Email'];
+	$message = $_POST['Message'];
+
+	$email_form = 'sajibuzzaman.fly@gmail.com';
+	$email_subject = "New Form Submission";
+	$email_body = "User Name: $name.\n".
+			"User Email: $visitor_email.\n".
+				"User Message: $message.\n;
+
+	$to = "sajibuzzaman.butterfly@gmail.com";
+	$headers = "From: $email_from \r\n";
+	$headers .= "Reply-To: $visitor_email \r\n";
+	mail($to,$email_subject,$email_body,$headers);
+	header("Location: contact.html");
+
+
+?>
